4f66a973c10903e85081c56c2ea62301ff2e8d5a
As'adur Rofiq <145300940+Rofiq02bae@users.noreply.github.com>
2025-11-08 06:54:19 +0000

    Revert "fix: Remove merge conflict markers from migration files"
    This reverts commit b5e7924e1b424f829796eda4631190d77d3aedd9.

--- jokiin/database/migrations/2025_11_06_032854_create_jokis_table.php
+++ jokiin/database/migrations/2025_11_06_032854_create_jokis_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('jokis', function (Blueprint $table) {
             $table->id();
+<<<<<<<< HEAD:jokiin/database/migrations/2025_11_04_010956_create_jokis_table.php
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_matkul')->constrained('matkuls')->onDelete('cascade');
+            $table->string('status');
+            $table->decimal('harga', 10, 2);
+            $table->timestamps();
+========
             $table->unsignedBigInteger('id_user');
             $table->unsignedBigInteger('id_matkul');
             $table->integer('harga');
@@ -21,6 +28,7 @@ return new class extends Migration
 
             $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('id_matkul')->references('id')->on('matkuls')->onDelete('cascade');
+>>>>>>>> dev-react:jokiin/database/migrations/2025_11_06_032854_create_jokis_table.php
         });
     }
 
--- jokiin/database/migrations/2025_11_06_032958_create_users_table.php
+++ jokiin/database/migrations/2025_11_06_032958_create_users_table.php
@@ -11,10 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('matkuls', function (Blueprint $table) {
             $table->id();
+<<<<<<<< HEAD:jokiin/database/migrations/2025_11_04_011002_create_matkuls_table.php
+            $table->string('nama_matkul');
+            $table->date('deadline');
+            $table->text('deskripsi');
+========
             $table->string('nama');
             $table->string('nomor');
+>>>>>>>> dev-react:jokiin/database/migrations/2025_11_06_032958_create_users_table.php
             $table->timestamps();
         });
     }
@@ -24,6 +30,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+<<<<<<<< HEAD:jokiin/database/migrations/2025_11_04_011002_create_matkuls_table.php
+        Schema::dropIfExists('matkuls');
+========
         Schema::dropIfExists('users');
+>>>>>>>> dev-react:jokiin/database/migrations/2025_11_06_032958_create_users_table.php
     }
 };
